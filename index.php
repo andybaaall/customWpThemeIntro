e2155bb17c1bcf5fd5d7a33ac06be5d6cf6c3d9c
Andy Ball <932400399@bun2-206-11.local>
2019-10-21 11:37:11 +1300

    added super basic Loop to index.php

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
 // echo('Hello, world!');
-
-
  ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+ 	<head>
+ 		<meta charset="utf-8">
+ 		<title>Hello, world!</title>
+
+		<?php wp_head();?>
+ 	</head>
+ 	<body>
+
+		<?php if ( have_posts() ): ?>
+			<?php while( have_posts() ): the_post(); ?>
+				<h1><?php the_title(); ?></h1>
+
+				<div>
+					<?php the_content(); ?>
+				</div>
+			<?php endwhile; ?>
+		<?php endif; ?>
+
+		<?php wp_footer(); ?>
+ 	</body>
+ </html>
