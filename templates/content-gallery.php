f17ef3a0131a237786ed7c361b211b8a065156d5
home <andybaaall>
2019-10-29 20:33:20 +1300

    just edited php.ini at home, toolbar not showing  on home pg only

--- templates/content-gallery.php
+++ templates/content-gallery.php
@@ -3,7 +3,21 @@
 		<div class="card-title">
 			<?php the_title(); ?>
 		</div>
-		<?php echo the_content(); ?>
+		<?php
+		if (has_blocks()) {
+			$allBlocks = parse_blocks(get_the_content());
+			// turning the content into an associative array
+
+			for ($i=0; $i < count($allBlocks); $i++) {
+				if ($allBlocks[$i]['blockName'] === 'core/gallery') {
+					$firstGalleryBlock = $allBlocks[$i];
+					break;
+			}
+		}
+		 ?>
+
+		
+		</div>
 		<a href="<?php the_permalink(); ?>" class="btn btn-round btn-danger">view gallery</a>
 
 	</div>
